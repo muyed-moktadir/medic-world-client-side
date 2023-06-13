c96ddd9da22440d4c782d697336f36b04d1b994f
muyed moktadir <muyed004@gmail.com>
2023-06-13 14:33:55 +0600

    contact us completed

--- src/pages/Home/Conntact Us/ContactUs.jsx
+++ src/pages/Home/Conntact Us/ContactUs.jsx
@@ -1,28 +1,36 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import { CustomButton } from "../InfoCards/InfocardStyle";
+import { backgrndImage } from "./ContactusStyle";
 
 const ContactUs = () => {
   return (
-    <div className="hero bg-base-200">
+    <div className="hero bg-base-200" style={backgrndImage}>
       <div className="card flex-shrink-0 w-full max-w-sm my-10">
         <div className="card-body">
+          <div className=" text-center mb-4">
+            <h5 className="text-primary text-center">Conatct Us</h5>
+            <h1 className="text-white text-2xl text-center">hello From the world</h1>
+          </div>
           <div className="form-control">
             <input
-              type="text"
-              placeholder="email"
+              type="mail"
+              placeholder="your email"
               className="input input-bordered"
             />
           </div>
           <div className="form-control">
             <input
               type="text"
-              placeholder="password"
+              placeholder="Subject"
               className="input input-bordered"
             />
           </div>
           <div className="form-control">
-            <textarea className="textarea textarea-accent" placeholder="Bio"></textarea>
+            <textarea
+              className="textarea textarea-accent"
+              placeholder="Bio"
+            ></textarea>
           </div>
           <div className="form-control w-[50%] mx-auto mt-3">
             <CustomButton>Submit</CustomButton>
---
+++ src/pages/Home/Conntact Us/ContactusStyle.jsx
@@ -0,0 +1,8 @@
+import bgImage from "../../../assets/images/appointment.png"
+
+
+export const backgrndImage = {
+    backgroundImage:`url(${bgImage})`,
+    backgroundRepeat :"no-repeat",
+    backgroundSize:"cover"
+}
