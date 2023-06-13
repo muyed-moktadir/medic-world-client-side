3c7e9c7d0d51221a4a50e811cfc6450138cb5b84
muyed moktadir <muyed004@gmail.com>
2023-06-13 09:35:19 +0600

    contact us component

---
+++ src/pages/Home/Conntact Us/ContactStyle.jsx
@@ -0,0 +1,8 @@
+import appointment from "../../../assets/images/appointment.png"
+
+
+export const ContactbgImage ={
+    backgroundImage:`url(${appointment})`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+}
--- src/pages/Home/Conntact Us/ContactUs.jsx
+++ src/pages/Home/Conntact Us/ContactUs.jsx
@@ -1,28 +1,36 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import { CustomButton } from "../InfoCards/InfocardStyle";
-
+import { CustomButton } from "../InfoCards/InfocardStyle"
+import { ContactbgImage } from "./ContactStyle";
 const ContactUs = () => {
   return (
-    <div className="hero bg-base-200">
-      <div className="card flex-shrink-0 w-full max-w-sm my-10">
+    <div className="hero bg-base-200" style={ContactbgImage}>
+      
+      <div>
+     
+      </div>
+      <div className="card flex-shrink-0  w-full max-w-sm my-10">
+       
+       
         <div className="card-body">
+        <h5 className="text-primary">Contact US</h5>
+        <h1 className="text-2xl">Stay connected with us</h1>
           <div className="form-control">
             <input
-              type="text"
-              placeholder="email"
+              type="email"
+              placeholder="Your email"
               className="input input-bordered"
             />
           </div>
           <div className="form-control">
             <input
               type="text"
-              placeholder="password"
+              placeholder="Subject"
               className="input input-bordered"
             />
           </div>
           <div className="form-control">
-            <textarea className="textarea textarea-accent" placeholder="Bio"></textarea>
+            <textarea className="textarea textarea-accent" placeholder="Your message"></textarea>
           </div>
           <div className="form-control w-[50%] mx-auto mt-3">
             <CustomButton>Submit</CustomButton>
